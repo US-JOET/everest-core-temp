0a03da0df8455dff53b5c58acdc3225c271beca4
Gianfranco Berardi <54074967+gberardi-pillar@users.noreply.github.com>
2023-12-05 11:42:50 -0600

    Turn off tests for now.
    Signed-off-by: Gianfranco Berardi <54074967+gberardi-pillar@users.noreply.github.com>

--- .ci/build-kit/compile.sh
+++ .ci/build-kit/compile.sh
@@ -6,6 +6,7 @@ cmake \
     -B build \
     -S "$EXT_MOUNT/source" \
     -G Ninja \
+    -DBUILD_TESTING=OFF \
     -DEVC_ENABLE_CCACHE=1 \
     -DISO15118_2_GENERATE_AND_INSTALL_CERTIFICATES=OFF \
     -DCMAKE_INSTALL_PREFIX="$WORKSPACE_PATH/dist"
