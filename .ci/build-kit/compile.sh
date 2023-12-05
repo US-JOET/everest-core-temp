c64400379dda07767f4f85e335aeaa0a30e460c0
Gianfranco Berardi <54074967+gberardi-pillar@users.noreply.github.com>
2023-12-05 11:56:52 -0600

    Build and test two step.
    Signed-off-by: Gianfranco Berardi <54074967+gberardi-pillar@users.noreply.github.com>

--- .ci/build-kit/compile.sh
+++ .ci/build-kit/compile.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-apk update && apk add gcovr gtest-dev && apk add --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/ lcov
-
 cmake \
     -B build \
     -S "$EXT_MOUNT/source" \
     -G Ninja \
-    -DBUILD_TESTING=OFF \
     -DEVC_ENABLE_CCACHE=1 \
     -DISO15118_2_GENERATE_AND_INSTALL_CERTIFICATES=OFF \
     -DCMAKE_INSTALL_PREFIX="$WORKSPACE_PATH/dist"
 
-ninja -j$(nproc) -C build
+ninja -j$(nproc) -C build install
